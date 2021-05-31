1202a9dec4a71fb02358f782d7bbcba536c724d0
Andres Icedo <andresicedo24@gmail.com>
2021-05-31 16:02:46 -0500

    +9

---
+++ codeWars/simpleEncryption.js
@@ -0,0 +1,52 @@
+/*
+For building the encrypted string:
+Take every 2nd char from the string, then the other chars, that are 
+not every 2nd char, and concat them as new String.
+Do this n times!
+
+Examples:
+
+"This is a test!", 1 -> "hsi  etTi sats!"
+"This is a test!", 2 -> "hsi  etTi sats!" -> "s eT ashi tist!"
+Write two methods:
+
+function encrypt(text, n)
+function decrypt(encryptedText, n)
+For both methods:
+If the input-string is null or empty return exactly this value!
+If n is <= 0 then return the input text.
+*/
+
+function encrypt(text, n) {
+    if (!text || n <= 0) return text; 
+    while (n--) {
+      let ans = '';
+      for (let i = 1; i < text.length; i += 2) {
+        ans += text[i];
+      }
+      for (let i = 0; i < text.length; i += 2) {
+        ans += text[i];
+      }
+      text = ans;
+    }
+    return text;
+  }
+  
+  function decrypt(encryptedText, n) {
+    if (!encryptedText || n <= 0) return encryptedText;
+    const ans = new Array(encryptedText.length);
+    while (n--) {
+      let j = 0;
+      for (let i = 1; i < ans.length; i += 2) {
+        ans[i] = encryptedText[j++];
+      }
+      for (let i = 0; i < ans.length; i += 2) {
+        ans[i] = encryptedText[j++];
+      }
+      encryptedText = ans.join('');
+    }
+    return encryptedText;
+  }
+
+console.log(encrypt("This is a test!", 1))// -> "hsi  etTi sats!"
+console.log(encrypt("This is a test!", 2))// -> "hsi  etTi sats!" -> "s eT ashi tist!"
