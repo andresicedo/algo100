dfbd1428a9317777fed22e193b57f83270aa6010
Andres Icedo <andresicedo24@gmail.com>
2021-07-21 20:32:07 -0500

    +1

--- leetCode/easy.js
+++ leetCode/easy.js
@@ -255,76 +255,98 @@
 // plane1.accelerate(); // console output: Boeing 787 going at 586 mi/h
 // plane2.decelerate(); // console output: Airbus A320 going at 649 mi/h
 
-/** Q3 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * Say you have an array prices for which the ith element is the price of a given 
- * stock on day i.
- * Design an algorithm to find the maximum profit. 
- * You may complete as many transactions as you like (i.e., buy one and sell one 
- * share of the stock multiple times).
- * Note: You may not engage in multiple transactions at the same time (i.e., you 
- * must sell the stock before you buy again).
- * 
- * Example 1:
- * Input: [7,1,5,3,6,4]
- * Output: 7
- * Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), 
- *              profit = 5-1 = 4.
-                Then buy on day 4 (price = 3) and sell on day 5 (price = 6), 
-                profit = 6-3 = 3.
- * 
- * Example 2:
- * Input: [1,2,3,4,5]
- * Output: 4
- * Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
-            Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
-            engaging multiple transactions at the same time. You must sell before buying again.
- *
- * Example 3:
- * Input: [7,6,4,3,1]
- * Output: 0
- * Explanation: In this case, no transaction is done, i.e. max profit = 0.
- */
-// complete the solution: 
-const prices = [7, 1, 5, 3, 6, 4, 6, 5, 7, 2, 4, 7];
-function solution(prices) {
-    let profit = 0;
-    for (let i = 0; i < prices.length; i++) {
-        const currPrice = prices[i];
-        const nextDayPrice = prices[i+1];
-        if (nextDayPrice > currPrice) profit+=(nextDayPrice-currPrice);
-    }
-    return profit;
-}
+// /** Q3 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//  * Say you have an array prices for which the ith element is the price of a given 
+//  * stock on day i.
+//  * Design an algorithm to find the maximum profit. 
+//  * You may complete as many transactions as you like (i.e., buy one and sell one 
+//  * share of the stock multiple times).
+//  * Note: You may not engage in multiple transactions at the same time (i.e., you 
+//  * must sell the stock before you buy again).
+//  * 
+//  * Example 1:
+//  * Input: [7,1,5,3,6,4]
+//  * Output: 7
+//  * Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), 
+//  *              profit = 5-1 = 4.
+//                 Then buy on day 4 (price = 3) and sell on day 5 (price = 6), 
+//                 profit = 6-3 = 3.
+//  * 
+//  * Example 2:
+//  * Input: [1,2,3,4,5]
+//  * Output: 4
+//  * Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+//             Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+//             engaging multiple transactions at the same time. You must sell before buying again.
+//  *
+//  * Example 3:
+//  * Input: [7,6,4,3,1]
+//  * Output: 0
+//  * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+//  */
+// // complete the solution: 
+// const prices = [7, 1, 5, 3, 6, 4, 6, 5, 7, 2, 4, 7];
+// function solution(prices) {
+//     let profit = 0;
+//     for (let i = 0; i < prices.length; i++) {
+//         const currPrice = prices[i];
+//         const nextDayPrice = prices[i+1];
+//         if (nextDayPrice > currPrice) profit+=(nextDayPrice-currPrice);
+//     }
+//     return profit;
+// }
+// // test:
+// console.log(solution(prices)); // expected: 16
 
-// test:
-console.log(solution(prices)); // expected: 16
+// /** Q4 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//  * Given a string s containing just the characters 
+//  * '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+//  * An input string is valid if:
+//  * Open brackets must be closed by the same type of brackets.
+//  * Open brackets must be closed in the correct order.
+//  * 
+//  * Example 1:
+//  * Input: s = "()"
+//  * Output: true
+//  * 
+//  * Example 2:
+//  * Input: s = "()[]{}"
+//  * Output: true
+//  * 
+//  * Example 3: 
+//  * Input: s = "(]"
+//  * Output: false
+//  */
+// // complete the solution:
+// function solution(str) {
+//     //base case
+//     if (str.length % 2 !== 0) return false;
+//     //identify the associated pairs in an obj literal
+//     let closedBrackets = {
+//         ")": "(",
+//         "}": "{",
+//         "]": "["
+//     };
+//     let openBrackets = [];
+//     for (let i = 0; i < str.length; i++) {
+//         const val = str[i];
+//         //if val is an open bracket,push to array
+//         if (val === "(" || val === "[" || val === "{") {
+//             openBrackets.push(val);
+//             //if a closing bracket matches with keys of obj
+//             //pop open bracket from the arr 
+//             //If there is a mismatch, return false.
+//         } else if (openBrackets[openBrackets.length - 1] === closedBrackets[val]) {
+//             openBrackets.pop();
+//         } else return false;
+//     }
+//     //should have no remaining values in open bracket array if string is balanced
+//     return openBrackets.length ? false : true;
+// }
 
-//    /** Q4 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//     * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-//     * An input string is valid if:
-//     * Open brackets must be closed by the same type of brackets.
-//     * Open brackets must be closed in the correct order.
-//     * 
-//     * Example 1:
-//     * Input: s = "()"
-//     * Output: true
-//     * 
-//     * Example 2:
-//     * Input: s = "()[]{}"
-//     * Output: true
-//     * 
-//     * Example 3: 
-//     * Input: s = "(]"
-//     * Output: false
-//     */
-//    // complete the solution:
-//    function solution(str) {
-
-//    }
-
-//    // test:
-//    console.log(solution("([]([{()}()]){})")); // true
-//    console.log(solution("([]([()}()]){})")); // false
-//    console.log(solution("([[]]([()}()]){})")); // false
+// // test:
+// console.log(solution("([]([{()}()]){})")); // true
+// console.log(solution("([]([()}()]){})")); // false
+// console.log(solution("([[]]([()}()]){})")); // false
 
 
