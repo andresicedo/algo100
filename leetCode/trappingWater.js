8895203b8509a3e796e7124ecd02137f52948f65
Andres Icedo <andresicedo24@gmail.com>
2021-07-22 15:40:51 -0500

    +1

--- leetCode/trappingWater.js
+++ leetCode/trappingWater.js
@@ -1,23 +1,26 @@
 let trap = (arr) => {
     //base case min 3 num required to trap water
     if (arr.length < 3) return 0;
-    //traverse arr to find left max and right max values for each idx
-    let leftMaxArr = [0];
-    let rightMaxArr = [0];
-    let currMax = arr[0];
+    let trappedWater = 0;
+    let leftMaxArr = [];
+    let rightMaxArr = [];
+    leftMaxArr[0] = arr[0];
     for (let i = 1; i < arr.length; i++) {
-        const currNum = arr[i];
-        currMax = Math.max(currNum, currMax);
-        leftMaxArr.push(currMax);
-    }
-    currMax = arr[arr.length -1];
-    for (let i = arr.length -2; i >= 0; i--) {
-        const currNum = arr[i];
-        currMax = Math.max(currMax, currNum);
-        rightMaxArr.push(currMax)
-    }
-    return leftMaxArr;
+        let currVal = arr[i];
+        leftMaxArr[i] = Math.max(currVal, leftMaxArr[i -  1]);
+    }//max left num at each index
+    rightMaxArr[arr.length - 1] = arr[arr.length - 1];
+    for (let i = arr.length - 2; i >= 0; i--) {
+        let currVal = arr[i];
+        rightMaxArr[i] = Math.max(currVal, rightMaxArr[i + 1])
+    }//max right num at each index
+    for (let i = 1; i < arr.length - 1; i++) {
+        let currVal = arr[i];
+        trappedWater += Math.min(leftMaxArr[i], rightMaxArr[i]) - currVal;
+    }//add difference between smallest of left/right values and currVal to trapped water
+    return trappedWater;
 }
 
 console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1]));//6
 console.log(trap([4,2,0,3,2,5]));//9
+console.log(trap([4,2,0,3,5,1]))//7
