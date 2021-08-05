e8ecd1ab298d1bfab69cb62472556347e4f2c50a
Andres Icedo <andresicedo24@gmail.com>
2021-08-04 20:51:42 -0500

    +2

---
+++ leetCode/Arrays/hardJumbledNumbers.js
@@ -0,0 +1,27 @@
+let words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+let alphabet = ' abcdefghijklmnopqrstuvwxyz'.split("");
+
+
+let jumbledNums = (string) => {
+    let result = [];
+    let substring = getAllSubstrings(string);
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        if (substring.includes(word)) {
+            result.push(words.indexOf(word));
+        }
+    }
+    return result;
+}
+
+let getAllSubstrings = (string) => {
+    let i, j, result = [];
+    for (i = 0; i < string.length; i++) {
+        for (j = i + 1; j < string.length + 1; j++) {
+            result.push(string.slice(i, j));
+        }
+    }
+    return result;
+}
+
+console.log(jumbledNums('eonenourfirz'));
